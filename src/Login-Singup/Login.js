c9c618bd2a80f0d3eb60f0854e959fc365c60bc4
Ayushi Gupta <ayushi.gupta2@incedonic.com>
2023-11-03 14:16:42 +0530

    validation complete

--- src/Login-Singup/Login.js
+++ src/Login-Singup/Login.js
@@ -1,60 +1,90 @@
-import axios from '../Api.js';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from '../Api.js';
+import '../Login-Singup/Login.css';
 
-import '../Login-Singup/Login1.css'
 function LoginPage() {
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
   const navigate = useNavigate();
-  
+
+  useEffect(() => {
+    // Clear error messages when email or password changes
+    setEmailError('');
+    // setPasswordError('');
+  }, [email]);
+  useEffect(()=>{
+    setPasswordError('')
+  },[password]);
+
+  function validateForm() {
+    let isValid = true;
+
+    // Validate email
+    if (!email) {
+      setEmailError('Email is required');
+      isValid = false;
+    }
+
+    // Validate password
+    if (!password) {
+      setPasswordError('Password is required');
+      isValid = false;
+    }
+
+    return isValid;
+  }
 
   function loginFun(e) {
     e.preventDefault();
 
-    axios.post("/login",
-      {
-        email: email,
-        password: password,
-      },{withCredentials:true})
-      .then((response) => {
-        if (response.status === 200) {
-          localStorage.setItem("user", email);
-          navigate('/home', );
-        }
-      })
-      .catch((err) => {
-        alert("Check again");
-      })
+    if (validateForm()) {
+      axios
+        .post('/login', { email, password }, { withCredentials: true })
+        .then((response) => {
+          if (response.status === 200) {
+            localStorage.setItem('user', email);
+            navigate('/home');
+          }
+        })
+        .catch((err) => {
+          alert('Check again');
+        });
+    }
   }
-
-
 
   return (
     <>
-      <div class="center">
+      <div className="center">
         <h1>Login</h1>
         <form method="post">
-          <div class="txt_field">
-            <input type="text" required onChange={e => setEmail(e.target.value)}></input>
+          <div className={`txt_field ${emailError && 'error'}`}>
+            <input type="text" required onChange={(e) => setEmail(e.target.value)} />
             <span></span>
-            <label> Email </label>
+            <label>Email</label>
+    
           </div>
-          <div class="txt_field">
-            <input type="password" required onChange={e => setPassword(e.target.value)}></input>
+          {emailError && <div className="error-message">{emailError}</div>}
+          <div className={`txt_field ${passwordError && 'error'}`}>
+            <input type="password" required onChange={(e) => setPassword(e.target.value)} />
             <span></span>
-            <label>Password </label>
+            <label>Password</label>
+         
           </div>
-          <div class="pass">Forgot Password?</div>
-          <button class="login-button" onClick={loginFun}> Login </button>
-          <div class="signup_link">
+          {passwordError && <div className="error-message">{passwordError}</div>}
+          <div className="pass">Forgot Password?</div>
+          <button className="login-button" onClick={loginFun}>
+            Login
+          </button>
+          <div className="signup_link">
             Not a member? <a href="/signup">Signup</a>
           </div>
         </form>
       </div>
     </>
-  )
+  );
 }
 
 export default LoginPage;
--- src/Login-Singup/Register.js
+++ src/Login-Singup/Register.js
@@ -1,23 +1,73 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from '../Api.js';
 import { useNavigate } from 'react-router-dom';
-import '../Login-Singup/Login1.css';
+import '../Login-Singup/Login.css';
 function SignupPage() {
   //states
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPass, setconfirmPass] = useState();
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [confirmPassError, setconfirmPassError] = useState();
   const navigate = useNavigate();
 
+
+  useEffect(()=>{
+    setEmailError('');
+  },[email])
+  
+  useEffect(()=>{
+    setPasswordError('')
+  },[password])
+useEffect(()=>{
+  setconfirmPassError('')
+},[confirmPass])
   function SignupFun(e) {
     e.preventDefault();
 
+
+
+    function validateForm() {
+      let isValid = true;
+
+
+      if (!email) {
+        setEmailError('Email is required');
+        isValid = false;
+      }
+      if (!password) {
+        setPasswordError('Password is required')
+      }
+      if (!confirmPass) {
+        setconfirmPassError('confirm password is required')
+      }
+  return isValid;
+    }
+   
+    if(validateForm()){
+
+      let isValid = true;
+
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setEmailError('Invalid email address');
+      isValid = false;
+    }
+
+    // Password validation (at least 8 characters)
+    if (password.length < 8) {
+      setPasswordError('Password must be at least 8 characters long');
+      isValid = false;
+    }
     if (password !== confirmPass) {
       alert("Password not matching");
       return;
     }
     else {
       console.log("Email:", email);
+
+     
       axios.post("/Signup",
         {
           email: email,
@@ -28,14 +78,17 @@ function SignupPage() {
           if (response.status === 200) {
             localStorage.setItem("user", email);
             navigate('/login');
-           
+
           }
-         
+
         })
         .catch((err) => {
           alert("Check again");
-        })
+        });
+      }
     }
+
+   
   }
 
   return (
@@ -48,16 +101,22 @@ function SignupPage() {
             <span></span>
             <label>Username</label>
           </div>
+          {emailError && <div className='error-message'>{emailError} </div>}
+
           <div class="txt_field">
             <input type="password" required onChange={e => setPassword(e.target.value)}></input>
             <span></span>
             <label>Password</label>
           </div>
+          {passwordError && <div className='error-message'>{passwordError}</div>}
+          
           <div class="txt_field">
             <input type="password" required onChange={e => setconfirmPass(e.target.value)}></input>
             <span></span>
             <label>confirm Password?</label>
           </div>
+          {confirmPassError &&<div className='error-message'>{confirmPassError}</div>}
+
           <button onClick={SignupFun} className='login-button'> signup </button>
           <div class="login_link">
             Already signed up? <a href="/login">Login</a>
